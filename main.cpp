eb5b9b5b55a810f905faf79b63fd78d2d47c27cc
Kolala <86872437+K0LALA@users.noreply.github.com>
2023-03-21 10:29:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-
+#include <algorithm>
 using namespace std;
 
-// Retourne vrai si n est un carré parfait
+// Retourne vrai si n est un carrÃ© parfait
 bool estCarreParfait(int n, const vector<int> & carres) {
     return find(carres.begin(), carres.end(), n) != carres.end();
 }
 
-// Créer une liste de nombres valides pour un nombre et inférieurs au max
+// CrÃ©er une liste de nombres valides pour un nombre et infÃ©rieurs au max
 vector<int> obtenirNombresOK(int max, int nombre, const vector<int>& carres)
 {
     vector<int> nombresOK;
 
     for (int i(0); i < carres.size(); ++i)
     {
-        // Si la différence du carré parfait par rapport au nombre actuel
-        // est supérieure au nombre maximum de la suite
+        // Si la diffÃ©rence du carrÃ© parfait par rapport au nombre actuel
+        // est supÃ©rieure au nombre maximum de la suite
         // OU
-        // est inférieure ou égale à 0
+        // est infÃ©rieure ou Ã©gale Ã  0
         // OU
-        // est égale à i
-        // On passe un prochain carré parfait
+        // est Ã©gale Ã  i
+        // On passe un prochain carrÃ© parfait
         if (carres[i] - nombre > max ||
             carres[i] - nombre <= 0 ||
             carres[i] - nombre == nombre) continue;
@@ -33,18 +33,18 @@ vector<int> obtenirNombresOK(int max, int nombre, const vector<int>& carres)
     return nombresOK;
 }
  
-// Renvoie true si la permutation envoyée est valide
+// Renvoie true si la permutation envoyÃ©e est valide
 bool estPermutationValide(const vector<int>& permutation, const vector<int> & carres) {
     // On prend la taille de la permutation
     int taille = permutation.size();
-    // Pour tous les binômes (d'où le taille - 1 car le dernier ne peut pas être avec le suivant)
+    // Pour tous les binÃ´mes (d'oÃ¹ le taille - 1 car le dernier ne peut pas Ãªtre avec le suivant)
     for (int i = 0; i < taille - 1; ++i) {
-        // On regarde si la somme des deux est un carré parfait
+        // On regarde si la somme des deux est un carrÃ© parfait
         if (!estCarreParfait(permutation[i] + permutation[i + 1], carres))
             // Si non on renvoie false
             return false;
     }
-    // Si on arrive ici, c'est que tous les binômes on été testés et sont valides car un return signifie qu'on sort de la fonction
+    // Si on arrive ici, c'est que tous les binÃ´mes on Ã©tÃ© testÃ©s et sont valides car un return signifie qu'on sort de la fonction
     return true;
 }
 
@@ -53,29 +53,29 @@ bool chercherPermutation(int n, vector<int>& permutation, vector<vector<int>>& n
     if (permutation.size() == n)
         return estPermutationValide(permutation, carres);
 
-    // On récupère le dernier de la suite
+    // On rÃ©cupÃ¨re le dernier de la suite
     int last = permutation.back();
     // Pour tous les nombres valides avec le dernier
     for (int i = 0; i < nombresOK[last - 1].size(); ++i) {
         if (nombresOK[last - 1][i] > n)
             break;
 
-        // On récupère le nombre i valide avec le dernier
+        // On rÃ©cupÃ¨re le nombre i valide avec le dernier
         int nombre = nombresOK[last - 1][i];
-        // Si ce nombre n'est pas déjà dans la suite actuelle
+        // Si ce nombre n'est pas dÃ©jÃ  dans la suite actuelle
         if (find(permutation.begin(), permutation.end(), nombre) == permutation.end()) {
-            // On l'ajoute à la suite
+            // On l'ajoute Ã  la suite
             permutation.push_back(nombre);
-            // On cherche une nouvelle permutation en renvoyant les mêmes paramètres que la fonction à reçu, juste avec la permutation changée
+            // On cherche une nouvelle permutation en renvoyant les mÃªmes paramÃ¨tres que la fonction Ã  reÃ§u, juste avec la permutation changÃ©e
             if (chercherPermutation(n, permutation, nombresOK, carres))
-                // Si une permutation a été trouvée, on renvoie true
+                // Si une permutation a Ã©tÃ© trouvÃ©e, on renvoie true
                 return true;
-            // On enlève le dernier élément de la permtation car cela signifie qu'on a pas pu finir la permutation avec le chox d'avant
+            // On enlÃ¨ve le dernier Ã©lÃ©ment de la permtation car cela signifie qu'on a pas pu finir la permutation avec le chox d'avant
             permutation.pop_back();
         }
     }
-    // Renvoie false si aucune permutation n'a été trouvée
-    // Comme vu au-dessus, si on renvoie false, on revient en arrière dans la suite
+    // Renvoie false si aucune permutation n'a Ã©tÃ© trouvÃ©e
+    // Comme vu au-dessus, si on renvoie false, on revient en arriÃ¨re dans la suite
     return false;
 }
 
@@ -98,7 +98,7 @@ int main() {
         cin >> max;
     } while (max < min);
 
-    // On définit les carrés parfaits jusqu'au plus haut possible (somme des deux maximum)
+    // On dÃ©finit les carrÃ©s parfaits jusqu'au plus haut possible (somme des deux maximum)
     vector<int> carresParfaits;
     for (int j(1); j <= sqrt(max + max - 1); ++j)
         carresParfaits.push_back(pow(j, 2));
@@ -112,12 +112,12 @@ int main() {
     // Pour tous les nombres dans la plage
     for (int i(min); i < max + 1; ++i)
     {
-        // On test avec chaque nombre comme départ
+        // On test avec chaque nombre comme dÃ©part
         for (int j(1); j < i + 1; ++j)
         {
-            // On créer notre suite (vide pour l'instant)
+            // On crÃ©er notre suite (vide pour l'instant)
             vector<int> suite = { j };
-            // Si la fonction chercherPermutation a réussi à trouver une permutation valide (elle renvoie true)
+            // Si la fonction chercherPermutation a rÃ©ussi Ã  trouver une permutation valide (elle renvoie true)
             if (chercherPermutation(i, suite, nombresOK, carresParfaits)) {
                 // On affiche la permutation
                 cout << "Une permutation valide pour " << i << " est : ";
